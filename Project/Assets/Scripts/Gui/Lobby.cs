a980b24dd2a029638a56f1e95eefe6e8202021b3
Angel Kyriako <angelkyriako@gmail.com>
2013-10-10 17:16:36 +0200

    started with gui

---
+++ Project/Assets/Scripts/Gui/Lobby.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lobby : MonoBehaviour {
+
+	public Texture2D babyDragons;
+
+	public int width;
+	public int height;	
+	public int x;
+	public int y;	
+	
+    void Start(){
+		width = 800;
+		height = 600;	
+		x = Screen.width/2-width/2;
+		y = Screen.height/2-height/2;	
+    }
+	
+	void OnGUI () {
+		GUI.Box(new Rect(x, y, width, height), new GUIContent(babyDragons));
+	}
+}
