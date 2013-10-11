b544c861729ebb51a2f35cd74ab589dead5e7cab
Angel Kyriako <angelkyriako@gmail.com>
2013-10-11 17:21:10 +0200

    Added entrance gui screen and started with lobby

---
+++ Project/Assets/Scripts/Lobby/MainLobby.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainLobby : MonoBehaviour {
+	
+    private string roomName;
+    private Vector2 scrollPos = Vector2.zero;	
+
+    void Awake()
+    {
+        if (!PhotonNetwork.connected)
+            PhotonNetwork.ConnectUsingSettings("v0.1");
+
+        PhotonNetwork.playerName = PlayerPrefs.GetString("playerName", "Guest" + Random.Range(1, 9999));		
+		
+		roomName = PhotonNetwork.playerName + " room";
+    }
+
+    void OnGUI()
+    {
+        if (!PhotonNetwork.connected){
+            ShowConnectingGUI();
+            return;
+        }
+
+
+        if (PhotonNetwork.room != null)
+            return;
+
+
+        GUILayout.BeginArea(new Rect((Screen.width - 400) / 2, (Screen.height - 300) / 2, 400, 300));
+
+        GUILayout.Label("Main Menu");
+
+        //Player name
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Player name:", GUILayout.Width(150));
+        PhotonNetwork.playerName = GUILayout.TextField(PhotonNetwork.playerName);
+        if (GUI.changed)//Save name
+            PlayerPrefs.SetString("playerName", PhotonNetwork.playerName);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(15);
+
+
+        //Join room by title
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("JOIN ROOM:", GUILayout.Width(150));
+        roomName = GUILayout.TextField(roomName);
+        if (GUILayout.Button("GO"))
+        {
+            PhotonNetwork.JoinRoom(roomName);
+        }
+        GUILayout.EndHorizontal();
+
+        //Create a room (fails if exist!)
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("CREATE ROOM:", GUILayout.Width(150));
+        roomName = GUILayout.TextField(roomName);
+        if (GUILayout.Button("GO"))
+        {
+            PhotonNetwork.CreateRoom(roomName, true, true, 10);
+        }
+        GUILayout.EndHorizontal();
+
+        //Join random room
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("JOIN RANDOM ROOM:", GUILayout.Width(150));
+        if (PhotonNetwork.GetRoomList().Length == 0)
+        {
+            GUILayout.Label("..no games available...");
+        }
+        else
+        {
+            if (GUILayout.Button("GO"))
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(30);
+        GUILayout.Label("ROOM LISTING:");
+        if (PhotonNetwork.GetRoomList().Length == 0)
+        {
+            GUILayout.Label("..no games available..");
+        }
+        else
+        {
+            //Room listing: simply call GetRoomList: no need to fetch/poll whatever!
+            scrollPos = GUILayout.BeginScrollView(scrollPos);
+            foreach (RoomInfo game in PhotonNetwork.GetRoomList())
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(game.name + " " + game.playerCount + "/" + game.maxPlayers);
+                if (GUILayout.Button("JOIN"))
+                {
+                    PhotonNetwork.JoinRoom(game.name);
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+        }
+
+        GUILayout.EndArea();
+    }
+
+
+    void ShowConnectingGUI()
+    {
+        GUILayout.BeginArea(new Rect((Screen.width - 400) / 2, (Screen.height - 300) / 2, 400, 300));
+
+        GUILayout.Label("Connecting to Photon server.");
+        GUILayout.Label("Hint: This demo uses a settings file and logs the server address to the console.");
+
+        GUILayout.EndArea();
+    }
+}
---
+++ Project/Assets/Scripts/Login/MainLogin.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MainLogin: MonoBehaviour{
+	
+	//GUI
+	private Rect fullScreenRect;
+	public Texture2D background;
+	
+	public Rect layoutRect;
+	public int spaceBig;	
+	public int spaceSmall;	
+	
+	public GUIStyle bigLabelStyle;
+	public GUIStyle smallLabelStyle;
+	public GUIStyle inputTextFieldStyle;
+	public GUIStyle buttonStyle;
+	
+	//Logic
+	private string playerName;
+
+
+	void Awake(){
+		playerName = "";
+		fullScreenRect = new Rect(0, 0, Screen.width, Screen.height);		
+		layoutRect = new Rect((Screen.width - layoutRect.width)/2, (Screen.height - layoutRect.height)/2,
+							   layoutRect.width, layoutRect.height);			
+	}
+	
+	void OnGUI(){
+        
+		GUI.DrawTexture(fullScreenRect, background, ScaleMode.StretchToFill);
+		
+		GUILayout.BeginArea(layoutRect);
+	        GUILayout.Label("Welcome to Dragongarden BITCH !!!", bigLabelStyle);
+			GUILayout.Space(spaceBig);
+			GUILayout.BeginVertical();
+				GUILayout.BeginHorizontal();
+			        GUILayout.Label("Enter name:", smallLabelStyle);
+					GUILayout.Space(spaceSmall);
+			        playerName = GUILayout.TextField(playerName, inputTextFieldStyle);
+			        if (GUI.changed)
+			            PlayerPrefs.SetString("playerName", playerName);
+				GUILayout.EndHorizontal();
+				GUILayout.Space(spaceBig);
+		        if (GUILayout.Button("Login", buttonStyle) && playerName.Trim().Length != 0)
+					Application.LoadLevel ("Lobby"); 			
+	        GUILayout.EndVertical();   					
+		GUILayout.EndArea();
+	}
+}
