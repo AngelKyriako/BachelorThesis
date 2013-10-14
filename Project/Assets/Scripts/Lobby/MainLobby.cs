0f34a7a5c8a8b8388613e61615acbcb983aa2371
Angel Kyriako <angelkyriako@gmail.com>
2013-10-14 20:20:09 +0200

    updated lobby gui

--- Project/Assets/Scripts/Lobby/MainLobby.cs
+++ Project/Assets/Scripts/Lobby/MainLobby.cs
@@ -1,118 +1,145 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainLobby : MonoBehaviour {
+public class MainLobby: MonoBehaviour{
 	
-    private string roomName;
-    private Vector2 scrollPos = Vector2.zero;	
-
-    void Awake()
-    {
+	private enum ButtonState{
+		none,
+		create,
+		join,
+		enterQueue
+	};
+	private ButtonState buttonPressedLast;
+    private string createRoomName, joinRoomName;
+    private Vector2 scrollPos = Vector2.zero;
+	private Rect fullscreen, north, south, west, east;
+	
+	
+	public int northHeight;
+	public int westWidth;	
+	public Texture2D background;
+	public GUIStyle titleStyle, welcomeLabelStyle, roomStyle;
+	public GUIStyle westButtonStyle, goButtonStyle, inputTextFieldStyle;
+	public int bigSpace;
+	public int smallSpace;
+	
+    void Awake(){
         if (!PhotonNetwork.connected)
-            PhotonNetwork.ConnectUsingSettings("v0.1");
-
-        PhotonNetwork.playerName = PlayerPrefs.GetString("playerName", "Guest" + Random.Range(1, 9999));		
+            PhotonNetwork.ConnectUsingSettings("v0.1");		
 		
-		roomName = PhotonNetwork.playerName + " room";
+		fullscreen = new Rect(0, 0, Screen.width, Screen.height);
+		north = new Rect(0, 0, Screen.width, northHeight);
+		south = new Rect(Screen.width, Screen.height, 0, 0);
+		west = new Rect(0, north.y + north.height,
+ 		  	   westWidth, Screen.height - (north.y + north.height));
+		east = new Rect(west.x + west.width, north.y + north.height,
+		  	   Screen.width - (west.x + west.width), Screen.height - (south.y - (north.y + north.height)));
+
+        PhotonNetwork.playerName = PlayerPrefs.GetString("name");
+		joinRoomName = "";
+		createRoomName = PhotonNetwork.playerName.EndsWith("s")?PhotonNetwork.playerName+"' room"
+														 	   :PhotonNetwork.playerName+"'s room";
+		buttonPressedLast = ButtonState.none;
     }
-
-    void OnGUI()
-    {
-        if (!PhotonNetwork.connected){
-            ShowConnectingGUI();
-            return;
-        }
-
-
-        if (PhotonNetwork.room != null)
-            return;
-
-
-        GUILayout.BeginArea(new Rect((Screen.width - 400) / 2, (Screen.height - 300) / 2, 400, 300));
-
-        GUILayout.Label("Main Menu");
-
-        //Player name
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Player name:", GUILayout.Width(150));
-        PhotonNetwork.playerName = GUILayout.TextField(PhotonNetwork.playerName);
-        if (GUI.changed)//Save name
-            PlayerPrefs.SetString("playerName", PhotonNetwork.playerName);
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(15);
-
-
-        //Join room by title
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("JOIN ROOM:", GUILayout.Width(150));
-        roomName = GUILayout.TextField(roomName);
-        if (GUILayout.Button("GO"))
-        {
-            PhotonNetwork.JoinRoom(roomName);
-        }
-        GUILayout.EndHorizontal();
-
-        //Create a room (fails if exist!)
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("CREATE ROOM:", GUILayout.Width(150));
-        roomName = GUILayout.TextField(roomName);
-        if (GUILayout.Button("GO"))
-        {
-            PhotonNetwork.CreateRoom(roomName, true, true, 10);
-        }
-        GUILayout.EndHorizontal();
-
-        //Join random room
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("JOIN RANDOM ROOM:", GUILayout.Width(150));
+	
+    private void ConnectingGUI(){
+    	GUILayout.BeginArea(new Rect((Screen.width - 400) / 2, (Screen.height - 300) / 2, 400, 300));
+        	GUILayout.Label("Connecting to Photon server.");
+        GUILayout.EndArea();
+    }	
+	
+	private void NorthGUI(){
+		GUILayout.BeginArea(north);
+		GUILayout.Space(bigSpace);	
+		GUILayout.Label("Dragonborn Lobby", titleStyle);
+		GUILayout.Label("Welcome " + PhotonNetwork.playerName, welcomeLabelStyle);
+		GUILayout.EndArea();
+	}
+	
+	private void SouthGUI(){
+		GUILayout.BeginArea(south);
+		GUILayout.EndArea();
+	}
+	
+	private void CreateRoomGUI(){
+        if (GUILayout.Button("Create", westButtonStyle))
+			buttonPressedLast = ButtonState.create;
+            //PhotonNetwork.CreateRoom(createRoomName, true, true, 10);
+	}
+	
+	private void JoinRoomGUI(){
+		GUILayout.BeginVertical();
+        joinRoomName = GUILayout.TextField(joinRoomName, inputTextFieldStyle);
+		GUILayout.Space(bigSpace);	
+        if (GUILayout.Button("Join", westButtonStyle))
+			buttonPressedLast = ButtonState.join;
+            //PhotonNetwork.JoinRoom(joinRoomName);
+		GUILayout.EndVertical();
+	}	
+	
+	private void EnterQueueGUI(){
+        if (GUILayout.Button("Enter queue", westButtonStyle))
+			buttonPressedLast = ButtonState.enterQueue;
+        	//PhotonNetwork.JoinRandomRoom();
+	}	
+	
+	private void RoomsGUI(){
         if (PhotonNetwork.GetRoomList().Length == 0)
-        {
-            GUILayout.Label("..no games available...");
-        }
-        else
-        {
-            if (GUILayout.Button("GO"))
-            {
-                PhotonNetwork.JoinRandomRoom();
-            }
-        }
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(30);
-        GUILayout.Label("ROOM LISTING:");
-        if (PhotonNetwork.GetRoomList().Length == 0)
-        {
-            GUILayout.Label("..no games available..");
-        }
-        else
-        {
-            //Room listing: simply call GetRoomList: no need to fetch/poll whatever!
+            GUILayout.Label("...no rooms available.", roomStyle);
+        else{
             scrollPos = GUILayout.BeginScrollView(scrollPos);
-            foreach (RoomInfo game in PhotonNetwork.GetRoomList())
-            {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label(game.name + " " + game.playerCount + "/" + game.maxPlayers);
-                if (GUILayout.Button("JOIN"))
-                {
-                    PhotonNetwork.JoinRoom(game.name);
-                }
-                GUILayout.EndHorizontal();
+            foreach (RoomInfo room in PhotonNetwork.GetRoomList()){
+                if (GUILayout.Button(room.name+" ("+room.playerCount+"/"+room.maxPlayers+")", roomStyle))
+					PhotonNetwork.JoinRoom(room.name);                    
             }
             GUILayout.EndScrollView();
-        }
-
-        GUILayout.EndArea();
-    }
-
-
-    void ShowConnectingGUI()
-    {
-        GUILayout.BeginArea(new Rect((Screen.width - 400) / 2, (Screen.height - 300) / 2, 400, 300));
-
-        GUILayout.Label("Connecting to Photon server.");
-        GUILayout.Label("Hint: This demo uses a settings file and logs the server address to the console.");
-
-        GUILayout.EndArea();
+        }		
+	}
+	
+	private void WestGUI(){
+		GUILayout.BeginArea(west);
+		CreateRoomGUI();
+		GUILayout.Space(bigSpace);	
+		EnterQueueGUI();
+		GUILayout.Space(3*bigSpace);	
+		RoomsGUI();
+		GUILayout.Space(bigSpace);	
+		JoinRoomGUI();		
+		GUILayout.EndArea();
+	}		
+	
+	private void CreatePropertiesGUI(){
+		GUILayout.Label("Create properties", goButtonStyle);
+	}
+	private void JoinPropertiesGUI(){
+		GUILayout.Label("Join properties", goButtonStyle);
+	}
+	private void QueuePropertiesGUI(){
+		GUILayout.Label("Queue properties", goButtonStyle);
+	}		
+	
+	private void EastGUI(){
+		GUILayout.BeginArea(east);
+		if (buttonPressedLast == ButtonState.join)
+			JoinPropertiesGUI();
+		else if(buttonPressedLast == ButtonState.create)
+			CreatePropertiesGUI();
+		else if(buttonPressedLast == ButtonState.enterQueue)
+			QueuePropertiesGUI();
+		GUILayout.EndArea();
+	}
+	
+    void OnGUI(){		
+        if (!PhotonNetwork.connected){
+			GUI.DrawTexture(fullscreen, background, ScaleMode.StretchToFill);
+            ConnectingGUI();
+		}
+        else if (PhotonNetwork.room == null){
+			GUI.DrawTexture(fullscreen, background, ScaleMode.StretchToFill);
+			NorthGUI();
+			SouthGUI();
+			WestGUI();
+			EastGUI();
+		}
     }
 }
--- Project/Assets/Scripts/Login/MainLogin.cs
+++ Project/Assets/Scripts/Login/MainLogin.cs
@@ -5,45 +5,40 @@ using System.Collections;
 public class MainLogin: MonoBehaviour{
 	
 	private string playerName;
-	private Rect fullScreenRect;
+	private Rect fullscreen;
 	
 	public Texture2D background;
 	public Rect layoutRect;
-	public GUIStyle bigLabelStyle;
-	public GUIStyle smallLabelStyle;
-	public GUIStyle inputTextFieldStyle;
-	public GUIStyle bigButtonStyle;	
-	public int spaceBig;	
-	public int spaceSmall;	
+	public GUIStyle bigLabelStyle, smallLabelStyle, inputTextFieldStyle, bigButtonStyle;	
+	public int bigSpace;	
+	public int smallSpace;	
 
 	void Awake(){
 		playerName = "";
-		fullScreenRect = new Rect(0, 0, Screen.width, Screen.height);		
+		fullscreen = new Rect(0, 0, Screen.width, Screen.height);
 		layoutRect = new Rect((Screen.width - layoutRect.width)/2, (Screen.height - layoutRect.height)/2,
 							   layoutRect.width, layoutRect.height);			
 	}
 	
 	void OnGUI(){
-        
-		GUI.DrawTexture(fullScreenRect, background, ScaleMode.StretchToFill);
+		GUI.DrawTexture(fullscreen, background, ScaleMode.StretchToFill);
 
 		GUILayout.BeginArea(layoutRect);
-			GUILayout.Space(spaceBig);
-			GUILayout.Space(spaceBig);				
-	        GUILayout.Label("Welcome to Dragon Garden", bigLabelStyle);
-			GUILayout.Space(spaceBig);
-			GUILayout.BeginVertical();
-				GUILayout.BeginHorizontal();
-			        GUILayout.Label("Enter name:", smallLabelStyle);
-					GUILayout.Space(spaceSmall);
-			        playerName = GUILayout.TextField(playerName, inputTextFieldStyle);
-			        if (GUI.changed)
-			            PlayerPrefs.SetString("playerName", playerName);
-				GUILayout.EndHorizontal();
-				GUILayout.Space(spaceBig);
-		        if (GUILayout.Button("Login", bigButtonStyle) && playerName.Trim().Length != 0)
-					Application.LoadLevel ("Lobby"); 			
-	        GUILayout.EndVertical();   					
+		GUILayout.Space(bigSpace*2);				
+        GUILayout.Label("Welcome to Dragonborn", bigLabelStyle);
+		GUILayout.Space(bigSpace);
+		GUILayout.BeginVertical();
+		GUILayout.BeginHorizontal();
+        GUILayout.Label("Enter name:", smallLabelStyle);
+		GUILayout.Space(smallSpace);
+        playerName = GUILayout.TextField(playerName, inputTextFieldStyle);
+        if (GUI.changed)
+            PlayerPrefs.SetString("name", playerName);
+		GUILayout.EndHorizontal();
+		GUILayout.Space(bigSpace);
+        if (GUILayout.Button("Login", bigButtonStyle) && playerName.Trim().Length != 0)
+			Application.LoadLevel ("Lobby"); 			
+        GUILayout.EndVertical();   					
 		GUILayout.EndArea();
 	}
 }
