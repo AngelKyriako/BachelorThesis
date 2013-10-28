755fc5cf681a38799350712db8ad73210eaf1638
Angel Kyriako <angelkyriako@gmail.com>
2013-10-28 13:15:56 +0100

    Bad fix on gravity problem
    TODO: make the character controller script awesome in order for
    everything to work without this shitty bugfix

--- Project/Assets/Scripts/MainGame/Characters/BabyDragon/Controllers/CharacterController.cs
+++ Project/Assets/Scripts/MainGame/Characters/BabyDragon/Controllers/CharacterController.cs
@@ -17,16 +17,16 @@ public class CharacterController: MonoBehaviour {
     }
 
     void Start() {
-        destination = transform.position;
+        animator.enabled = false;
+        distance = 0;
         currentSpeed = 0;
     }
 
     void Update() {
-        distance = Vector3.Distance(destination, transform.position);
 
         if (!Input.GetKey(KeyCode.LeftAlt) && !Input.GetKey(KeyCode.RightAlt) &&
             Input.GetMouseButtonDown(1) && GUIUtility.hotControl == 0) {
-
+            animator.enabled = true;
             Plane playerPlane = new Plane(Vector3.up, transform.position);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             float hitdist = 0.0f;
@@ -38,31 +38,14 @@ public class CharacterController: MonoBehaviour {
                 transform.rotation = targetRotation;
             }
             currentSpeed = movementSpeed;
+            distance = Vector3.Distance(destination, transform.position);
         }
         else
             currentSpeed = 0;
 
-        //// Moves the player if the mouse button is hold down
-        //else if (Input.GetMouseButton(1) && GUIUtility.hotControl == 0) {
-
-        //    Plane playerPlane = new Plane(Vector3.up, transform.position);
-        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //    float hitdist = 0.0f;
-
-        //    if (playerPlane.Raycast(ray, out hitdist)) {
-        //        Vector3 targetPoint = ray.GetPoint(hitdist);
-        //        destination = ray.GetPoint(hitdist);
-        //        Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-        //        transform.rotation = targetRotation;
-        //    }
-        //    //	transform.position = Vector3.MoveTowards(transform.position, destinationPosition, movementSpeed * Time.deltaTime);
-        //}
-
-        // To prevent code from running if not needed
-        if (distance > .5f) {
-            transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime);
-        }
         animator.SetFloat("movementSpeed", currentSpeed);
+        if (distance > .2f)
+            transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime);
     }
 
     public void SetIsLocal(bool b) { isLocal = b; }
