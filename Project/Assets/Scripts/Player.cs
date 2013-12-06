60f670b58e22a25bf18f5fa0cb6602fc84508f37
Angel Kyriako <angelkyriako@gmail.com>
2013-12-06 22:19:33 +0100

    -.-

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -1,34 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum PlayerTeam{
-    None,
-    Team1,
-    Team2,
-    Team3,
-    Team4,
-    Team5,
-    Team6,
-    Team7,
-    Team8,
-    Team9,
-    Team10,
-}
-
-public enum PlayerColor{
-    None,
-    Red,
-    Blue,
-    Green,
-    Purple,
-    Yellow,
-    Pink,
-    Orange,
-    Brown,
-    Gray,
-    Black
-}
-
 public class Player {
 
     private string name;
