99bd39759d6a13ea2300cd926d15b69aa80fd11d
Angel Kyriako <angelkyriako@gmail.com>
2013-12-17 04:22:35 +0100

    updated game over manager

--- Project/Assets/Scripts/Managers/GameOverManager.cs
+++ Project/Assets/Scripts/Managers/GameOverManager.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class GameOverManager: MonoBehaviour {
 
-	void Awake () {
-	
+    public Vector3 winnerSpot = Vector3.zero;
+    public int winnerSpotVariance = 5;
+
+    void Awake () {
+        winnerSpot += new Vector3(Random.Range(-winnerSpotVariance, winnerSpotVariance),
+                                  0,
+                                  Random.Range(-winnerSpotVariance, winnerSpotVariance));
+        enabled = false;
 	}
-	
-	void Update () {
-	
-	}
+
+    void SetUp(string _winnerId) {
+        foreach (string _id in GameManager.Instance.AllPlayerKeys)
+            if (CombatManager.Instance.IsAlly(_winnerId))
+                TeleportManager.Instance.TeleportToPoint(winnerSpot);
+        enabled = true;
+    }
 
     void OnGUI() {
         GUILayout.Button("Game Over");
