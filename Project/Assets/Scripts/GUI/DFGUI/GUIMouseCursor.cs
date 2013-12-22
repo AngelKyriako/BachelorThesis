8abdce6cb3bfa1bea81f36aff4f8c2162e4e7ad2
Angel Kyriako <angelkyriako@gmail.com>
2013-12-22 23:28:54 +0100

    fixed GUI cursor bug
    Need to add functionality for changing cursor at some point

---
+++ Project/Assets/Scripts/GUI/DFGUI/GUIMouseCursor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMouseCursor: SingletonMono<GUIMouseCursor> {
+
+    public string defaultCursor,
+                  horizontal,
+                  vertical,
+                  bottomLeftTopRight,
+                  bottomRightTopLeft;
+
+    private dfSprite sprite;
+
+    private GUIMouseCursor() { }
+
+    void Awake() {
+        sprite = GetComponent<dfSprite>();
+    }
+
+    void Start() {
+        // We don't want the drag cursor to intercept mouse messages
+        sprite.IsInteractive = false;
+        sprite.IsEnabled = false;
+
+        Screen.showCursor = false;
+        SetCursor(defaultCursor);
+    }
+
+    //@TODO: Set mouse cursor when at edges of screen or targeting
+
+    void Update() {
+        sprite.RelativePosition = sprite.GetManager().ScreenToGui(Input.mousePosition);
+        sprite.BringToFront();
+    }
+
+    public void SetCursor(string _sprite) {
+        sprite.SpriteName = _sprite;
+    }
+}
--- Project/Assets/Scripts/GUI/DFGUI/GUISkills/GUIDragCursor.cs
+++ Project/Assets/Scripts/GUI/DFGUI/GUISkills/GUIDragCursor.cs
@@ -2,20 +2,23 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class GUIDragCursor: GUICursor {
+public class GUIDragCursor: MonoBehaviour {
 
-    public override void Start() {
-        base.Start();
-        // We don't want the drag cursor visible unless it is being used
-        Sprite.Hide();
+    private static dfSprite sprite;
+    private static Vector2 cursorOffset;
 
+    void Start() {
+        // Obtain a reference to the Sprite that this component is attached to
+        sprite = GetComponent<dfSprite>();
         // We don't want the drag cursor to intercept mouse messages
-        Sprite.IsInteractive = false;
-        Sprite.IsEnabled = false;
+        sprite.IsInteractive = false;
+        sprite.IsEnabled = false;
+        // We don't want the drag cursor visible unless it is being used
+        sprite.Hide();
     }
 
-    public override void Update() {
-        if (Sprite.IsVisible)
+    void Update() {
+        if (sprite.IsVisible)
             SetPosition(Input.mousePosition);
     }
 
@@ -25,20 +28,27 @@ public class GUIDragCursor: GUICursor {
     /// <param name="sprite">The sprite to display in the drag cursor</param>
     /// <param name="position">The initial position of the drag cursor</param>
     /// <param name="offset">The mouse offset within the dragged object</param>
-    public static void Show(dfSprite sprite, Vector2 position, Vector2 offset) {
-        CursorOffset = offset;
+    public static void Show(dfSprite _sprite, Vector2 _position, Vector2 _offset) {
+        cursorOffset = _offset;
 
-        SetPosition(position);
+        SetPosition(_position);
 
-        Sprite.Size = sprite.Size;
-        Sprite.Atlas = sprite.Atlas;
-        Sprite.SpriteName = sprite.SpriteName;
-        Sprite.IsVisible = true;
-        Sprite.BringToFront();
+        sprite.Size = _sprite.Size;
+        sprite.Atlas = _sprite.Atlas;
+        sprite.SpriteName = _sprite.SpriteName;
+        sprite.IsVisible = true;
+        sprite.BringToFront();
 
     }
 
     public static void Hide() {
-        Sprite.IsVisible = false;
+        sprite.IsVisible = false;
+    }
+
+    private static void SetPosition(Vector2 position) {
+        // Convert position from "screen coordinates" to "gui coordinates"
+        position = sprite.GetManager().ScreenToGui(position);
+        // Center the control on the mouse/touch
+        sprite.RelativePosition = position - cursorOffset;
     }
 }
